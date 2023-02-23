77d5b32f62ae499d48f72874c0fb3a9c03ada16a
Artak <artak@nteams.com>
2023-02-23 17:48:00 +0400

    add relation on models, (PROCESSING)

--- app/Models/Workspace.php
+++ app/Models/Workspace.php
@@ -7,6 +7,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Service\Models\Entity\ServiceModel;
 
 class Workspace extends ServiceModel
@@ -55,5 +56,13 @@ class Workspace extends ServiceModel
     public function workers(): BelongsToMany
     {
         return $this->belongsToMany(User::class, Worker::class, 'user_id', 'workspace_id');
+    }
+
+    /**@TODO FIX THIS RELATION
+     * @return HasManyThrough
+     */
+    public function tasks(): HasManyThrough
+    {
+        return $this->hasManyThrough(Board::class, BoardTask::class);
     }
 }
