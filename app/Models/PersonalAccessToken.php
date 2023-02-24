6dc07d4a9eb0a87c2a0c6298a0cad853d0e431cb
Artak <artak@nteams.com>
2023-02-24 15:17:39 +0400

    add relation on models, (PROCESSING-1)

---
+++ app/Models/PersonalAccessToken.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class PersonalAccessToken extends \Laravel\Sanctum\PersonalAccessToken
+{
+    /**
+     * Get the tokenable model that the access token belongs to.
+     *
+     * @return BelongsTo
+     */
+    public function tokenable(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'user_id');
+    }
+}
