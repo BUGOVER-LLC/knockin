8ccdf987c886ef237effc18172fc765b7e5eec70
Artak <artak@nteams.com>
2023-02-24 18:04:22 +0400

    relation create processing

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -4,8 +4,41 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Service\Models\Entity\ServiceModel;
 
 class Message extends ServiceModel
 {
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'message_id';
+    /**
+     * @var string[]
+     */
+    protected $fillable = ['author_id', 'channel_id', 'workspace_id', 'parent_id', 'body'];
+
+    /**
+     * @return BelongsTo
+     */
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'author_id');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function channel(): BelongsTo
+    {
+        return $this->belongsTo(Channel::class, 'channel_id', 'channel_id');
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function workspace(): BelongsTo
+    {
+        return $this->belongsTo(Workspace::class, 'workspace_id', 'workspace_id');
+    }
 }
