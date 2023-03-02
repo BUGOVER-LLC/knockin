a12659dd1d89c0dc2d6add8ee950a3253241c983
Artak <artak@nteams.com>
2023-03-02 23:13:15 +0400

    starting basic complete auth and index start page

--- app/Repositories/WorkspaceRepository.php
+++ app/Repositories/WorkspaceRepository.php
@@ -4,11 +4,20 @@ declare(strict_types=1);
 
 namespace App\Repositories;
 
+use App\Models\Workspace;
+use Illuminate\Contracts\Container\Container;
 use Service\Repository\Repositories\BaseRepository;
 
 class WorkspaceRepository extends BaseRepository
 {
-    public function __construct()
+    /**
+     * @param Container $container
+     */
+    public function __construct(Container $container)
     {
+        $this->setContainer($container)
+            ->setModel(Workspace::class)
+            ->setCacheDriver('redis')
+            ->setRepositoryId(Workspace::getTableName());
     }
 }
