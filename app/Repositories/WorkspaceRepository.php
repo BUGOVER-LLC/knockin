e4f78f1f77e6c3ee45c8f5b41222c58c0bc22ca4
Artak <artak@nteams.com>
2023-03-02 23:04:34 +0400

    starting basic complete auth and index start page

---
+++ app/Repositories/WorkspaceRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repositories;
+
+use Service\Repository\Repositories\BaseRepository;
+
+class WorkspaceRepository extends BaseRepository
+{
+    public function __construct()
+    {
+    }
+}
