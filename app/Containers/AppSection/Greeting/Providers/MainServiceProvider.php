a74f05e78c0d34b4c49e32a0ea21e5384858b000
Artak <artak@nteams.com>
2023-06-17 17:41:26 +0400

    in processing

---
+++ app/Containers/AppSection/Greeting/Providers/MainServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Containers\AppSection\Greeting\Providers;
+
+class MainServiceProvider extends \Ship\Parents\Providers\MainServiceProvider
+{
+    /**
+     * Container Service Providers.
+     */
+    public array $serviceProviders = [
+    ];
+
+    /**
+     * Container Aliases
+     */
+    public array $aliases = [
+
+    ];
+}
