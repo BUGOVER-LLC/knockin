975dda5f5b6ac42f830baa346996240e7fad92e0
Artak <artak@nteams.com>
2023-06-17 17:06:40 +0400

    in processing

---
+++ app/Containers/AppSection/Greeting/UI/WEB/Routes/main.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Support\Facades\Route;
+
+
+//Route::group(
+//    ['middleware' => ['guest']],
+//    Route::get('/', '\Containers\AppSection\Greeting\UI\WEB\Controllers\GreetingIndexController')->name('greeting-index')
+//);
