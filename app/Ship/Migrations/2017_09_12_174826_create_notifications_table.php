bb23effc546c4b9db6e6d38041ce26a56e8d6bdc
artak <manukyan.artak.21@gmail.com>
2024-01-16 23:31:40 +0400

    in progress restructure all sections so that the picture is visible

--- app/Ship/Migrations/2017_09_12_174826_create_notifications_table.php
+++ app/Ship/Migrations/2017_09_12_174826_create_notifications_table.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,7 +14,7 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('notifications', function (Blueprint $table) {
+        Schema::connection('pgsql_app')->create('notifications', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('type');
             $table->morphs('notifiable');
