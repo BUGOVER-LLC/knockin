475c8f857df3ddef6fef6dd25501408c3c9e3b19
BUGOVER <manukyan.artak.21@gmail.com>
2024-11-16 22:39:05 +0400

    preparing on started production use

---
+++ app/ShipManager/Parents/Middlewares/Middleware.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ship\Parents\Middlewares;
+
+use Nucleus\Abstracts\Middlewares\Middleware as AbstractMiddleware;
+
+abstract class Middleware extends AbstractMiddleware
+{
+
+}
---
+++ app/ShipManager/Parents/Values/Value.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ship\Parents\Values;
+
+use Nucleus\Abstracts\Values\Value as AbstractValue;
+
+abstract class Value extends AbstractValue
+{
+}
